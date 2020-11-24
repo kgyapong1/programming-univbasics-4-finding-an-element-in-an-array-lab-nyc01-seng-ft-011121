24bea672b979a53f4994e8d2260f90284672d607
Kwaku Gyapong <kwakugyapong219@gmail.com>
2020-11-24 19:07:59 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   count = 0
   while count < array.length do
-    # comparison code will go here
+    ts array[count] == value_to_find# comparison code will go here
     count += 1
 end
 end
